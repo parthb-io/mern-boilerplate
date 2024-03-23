957b6e1cdc09d2d8541a957dc03f83d989431494
parthkumar9 <parthkumar9@outlook.com>
2024-03-23 00:18:21 -0600

    routes-fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <>
-      <div className="text-pink-500">hello auth</div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
